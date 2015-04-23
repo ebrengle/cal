29be3a20f7c2c79dae14d7664cbe57c4f5bd5afe
Eric Brengle <eric.brengle@gmail.com>
2015-04-23 14:17:47 -0500

    Beginning unit tests

--- cal.rb
+++ cal.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/env ruby
 
-# month = ARGV[0]
-# year = ARGV[1]
+require_realtive 'lib/month'
+
+month = ARGV[0]
+year = ARGV[1]
+
 # puts `cal #{month} #{year}`
+
+m = Month.new(month, year)
+puts m.to_s
