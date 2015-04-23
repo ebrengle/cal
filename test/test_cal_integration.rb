045dd6da95a454e91f0c87cb6665299e6b037463
Eric Brengle <eric.brengle@gmail.com>
2015-04-23 10:53:37 -0500

    Setup rakefile, got first test erroring out

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -3,9 +3,10 @@ require_relative 'helper'
 class TestCalIntegration < MiniTest::Test
 
   def test_helper_arguments
-    output = `./cal 12/13 2004`
+    output = `./cal 01/01 2012`
     expected = <<EOS
 
 EOS
   assert_equal expected, output
   end
+end
